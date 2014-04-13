245457e548ead8e39da46aae2bdc26985d30170b
Fabien Benureau <fabien.benureau+git@gmail.com>
2014-04-13 17:38:47 +0200

    ABC for Environment

--- explorers/envs/env.py
+++ explorers/envs/env.py
@@ -1,6 +1,24 @@
 from __future__ import absolute_import, division, print_function, unicode_literals
+import abc
 
 class Environment(object):
+    __metaclass__ = abc.ABCMeta
 
+    @abc.abstractproperty
+    def cfg(self):
+        raise NotImplementedError
+
+    @abc.abstractmethod
     def execute(self, order):
         raise NotImplementedError
+
+    @classmethod
+    def __subclasshook__(cls, C):
+        check = NotImplemented
+        if cls is Environment:
+            check = True
+            required = ['execute', 'cfg']
+            for method in required:
+                if not any(method in B.__dict__ for B in C.__mro__):
+                    check = NotImplemented
+        return check
--- tests/testenvs.py
+++ tests/testenvs.py
@@ -1,8 +1,11 @@
 from __future__ import absolute_import, division, print_function
 import random
 
+import forest
+
 import dotdot
 from explorers import envs
+
 
 class RandomEnv(envs.Environment):
 
@@ -12,12 +15,26 @@ class RandomEnv(envs.Environment):
                            envs.Channel('feedback1'),
                            envs.Channel('feedback3')]
 
+        self._cfg = forest.Tree()
+        self._cfg.m_channels = self.m_channels
+        self._cfg.s_channels = self.s_channels
+        self._cfg._freeze(True)
+
+    @property
+    def cfg(self):
+        return self._cfg
+
     def execute(self, order):
         return {'order':order,
                 'feedback':{c.name: random.random() for c in self.s_channels}}
+
 
 class BoundedRandomEnv(RandomEnv):
 
     def __init__(self, mbounds, sbounds):
         self.m_channels = [envs.Channel('order{}'.format(i), mb_i) for i, mb_i in enumerate(mbounds)]
         self.s_channels = [envs.Channel('feedback{}'.format(i), sb_i) for i, sb_i in enumerate(sbounds)]
+
+
+assert issubclass(RandomEnv, envs.Environment)
+assert issubclass(BoundedRandomEnv, envs.Environment)
