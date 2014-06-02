ebce8880674cde2ee7b921c17396cf54dc04a16c
Fabien Benureau <fabien.benureau+git@gmail.com>
2014-06-02 16:32:53 +0200

    GoalSetExplorer class

---
+++ explorers/algorithms/s_set.py
@@ -0,0 +1,46 @@
+"""\
+Goal are chosen amongst a predefined set. Useful for tests.
+"""
+from __future__ import absolute_import, division, print_function
+import random
+import collections
+
+import learners
+
+from .. import conduits
+from .. import explorer
+from .. import tools
+
+
+defcfg = explorer.RandomGoalExplorer.defcfg._copy(deep=True)
+defcfg._describe('s_channels', instanceof=collections.Iterable,
+                 docstring='Sensory channels to generate random goal from')
+defcfg._describe('s_goals', instanceof=collections.Iterable,
+                 docstring='Set of goals to choose from')
+defcfg._branch('learner')
+
+class GoalSetExplorer(explorer.RandomGoalExplorer):
+    """\
+    Goal are chosen amongst a predefined set. Useful for tests.
+    """
+    defcfg = defcfg
+
+    def __init__(self, cfg, inv_learners=(), **kwargs):
+        super(GoalSetExplorer, self).__init__(cfg)
+        self.s_goals = self.cfg.s_goals
+        self.cursor = 0
+
+    def explore(self):
+        if self.cursor < len(self.s_goals):
+            s_goal = self.s_goals[self.cursor]
+            self.cursor += 1
+            m_goal = self._inv_request(s_goal)
+            if m_goal is None:
+                m_goal = tools.avg_signal(self.m_channels)
+            return {'m_goal': m_goal, 's_goal': s_goal, 'from': 'goal.babbling.set'}
+
+    def _inv_request(self, s_goal):
+        orders = self.inv_conduit.poll({'s_goal': s_goal,
+                                        'm_channels': self.m_channels})
+        return None if len(orders) == 0 else random.choice(orders)
+
