8e872169c7daad577c71c90147195f19fc155891
Fabien Benureau <fabien.benureau+git@gmail.com>
2014-08-27 21:58:47 +0200

    RestrictExplorer for manipulating sensory range

--- explorers/__init__.py
+++ explorers/__init__.py
@@ -6,15 +6,16 @@ from .explorer import Explorer
 from .meshgrid import MeshGrid
 
 # Explorers
-from .algorithms.m_rand    import RandomMotorExplorer
-from .algorithms.s_rand    import RandomGoalExplorer
-from .algorithms.s_mesh    import MeshgridGoalExplorer
-from .algorithms.s_unreach import UnreachGoalExplorer
-from .algorithms.mix2      import Mix2Explorer
-from .algorithms.reuse     import ReuseExplorer
-from .algorithms.reuse     import MultiReuseExplorer
-from .algorithms.s_set     import GoalSetExplorer
-from .algorithms.meta      import MetaExplorer
+from .algorithms.m_rand     import RandomMotorExplorer
+from .algorithms.s_rand     import RandomGoalExplorer
+from .algorithms.s_mesh     import MeshgridGoalExplorer
+from .algorithms.s_unreach  import UnreachGoalExplorer
+from .algorithms.mix2       import Mix2Explorer
+from .algorithms.reuse      import ReuseExplorer
+from .algorithms.reuse      import MultiReuseExplorer
+from .algorithms.s_set      import GoalSetExplorer
+from .algorithms.meta       import MetaExplorer
+from .algorithms.s_restrict import RestrictGoalExplorer
 
 # Interest Model
 from .algorithms.im        import IMExplorer
