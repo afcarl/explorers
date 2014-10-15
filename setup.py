184ca6480e7f8425aafc04d4dfda435793fc8f85
Fabien Benureau <fabien.benureau+git@gmail.com>
2014-10-15 12:20:28 +0200

    update setup.py

--- setup.py
+++ setup.py
@@ -1,20 +1,28 @@
 import os
-from distutils.core import setup
+from setuptools import setup
 
 setup(
-    name = "explorers",
-    version = "0.2",
-    author = "Fabien Benureau",
+    name         = "explorers",
+    version      = "1.0",
+    author       = "Fabien Benureau",
     author_email = "fabien.benureau@inria.fr",
-    description = ("Framework for exploration algorithms"),
-    license = "Open Science.",
-    keywords = "exploration learning algorithm",
-    url = "flowers.inria.fr",
-    packages=['explorers',
-              'explorers.algorithms',
-              'explorers.algorithms.reuse',
-              'explorers.algorithms.im',
-                          ],
-    requires=['leaners'],
-    classifiers=[],
+    description  = 'Framework for autonomous exploration algorithms in sensorimotor spaces',
+    license      = "Open Science (see fabien.benureau.com/openscience.html",
+    keywords     = "exploration learning algorithm sensorimotor robots robotics",
+    url          = "flowers.inria.fr",
+    packages=[
+        'explorers',
+        'explorers.algorithms',
+        'explorers.algorithms.reuse',
+        'explorers.algorithms.im',
+    ],
+    dependency_links=[
+        "https://github.com/flowersteam/forest/tarball/master#egg=forest-1.0",
+    ],
+    install_requires=['forest', 'learners'],
+    classifiers=[
+        'Development Status :: 4 - Beta',
+        'Programming Language :: Python :: 2.7',
+        'Intended Audience :: Science/Research',
+    ]
 )
