9b1d1b669030ea9b6ef22eee767dc5ddc64e4550
Fabien Benureau <fabien.benureau+git@gmail.com>
2015-11-19 00:11:14 +0100

    version 1.2.0

--- setup.py
+++ setup.py
@@ -3,22 +3,24 @@ from setuptools import setup
 
 import versioneer
 
+VERSION = '1.2.0'
+
 setup(
     name         = "explorers",
-    version      = "1.1.0",
+    version      = VERSION,
     cmdclass     = versioneer.get_cmdclass(),
     author       = "Fabien Benureau",
-    author_email = "fabien.benureau@inria.fr",
+    author_email = "fabien.benureau@gmail.com",
     description  = 'Framework for autonomous exploration algorithms in sensorimotor spaces',
-    license      = "Open Science (see fabien.benureau.com/openscience.html",
+    license      = "Open Science License (see fabien.benureau.com/openscience.html",
     keywords     = "exploration learning algorithm sensorimotor robots robotics",
+    download_url = 'https://github.com/humm/explorers/tarball/{}'.format(VERSION),
     url          = "github.com/humm/explorers.git",
-    packages=[
-        'explorers',
-        'explorers.algorithms',
-        'explorers.algorithms.reuse',
-        'explorers.algorithms.im',
-    ],
+    packages=['explorers',
+              'explorers.algorithms',
+              'explorers.algorithms.reuse',
+              'explorers.algorithms.im',
+             ],
     install_requires=['numpy', 'scicfg', 'learners', 'shapely', 'environments'],
     classifiers=[
         'Development Status :: 4 - Beta',
